ecfe0cee8486526daec955360b7a1b5234cd4b18
User1012007 <rojasbadilloe@gmail.com>
2026-09-10 07:49:44 -0600

    main.cpp: structure of the functions, its missing format the file correctly and the stack

--- DSA/unidad-1/binomial/main.cpp
+++ DSA/unidad-1/binomial/main.cpp
@@ -1,30 +1,85 @@
 #include <iostream>
-// #include <tuple>
-// #include <vector>
+#include <stack>
+#include <tuple>
+#include <vector>
+
 using namespace std;
 
-// struct binomy {
-//   int potencia;
-//   tuple<int, string, string, string, string, int> state;
-// };
-//
-// vector<binomy> binomial;
+struct binomy {
+  int potencia;
+  stack<tuple<int, string, string, string, string, int>> state;
+};
 
-void pila() {}
+int counter = 0;
 
-int exponente(int n, int k) {
-  if (n == 0 || k == 0) {
+vector<binomy> binomial;
+
+int obtenerCoeficiente(int n, int k) {
+  if (k == 0 || k == n) {
     return 1;
-
-  } else {
-    exponente(n - 1, k - 1);
-    exponente(n - 1, k);
   }
+  return obtenerCoeficiente(n - 1, k - 1) + obtenerCoeficiente(n - 1, k);
 }
 
-void piramide() {}
+string formatearTermino(int coef, int expA, int expB, bool esPrimero) {
+  string resultado = "";
 
-void solve(int exponente) {}
+  if (!esPrimero) {
+    resultado += " + ";
+  }
+
+  if (coef > 1 || (expA == 0 && expB == 0)) {
+    resultado += to_string(coef);
+  }
+
+  if (expA == 1) {
+    resultado += "a";
+  } else if (expA > 1) {
+    resultado += "a^" + to_string(expA);
+  }
+
+  if (expB == 1) {
+    resultado += "b";
+  } else if (expB > 1) {
+    resultado += "b^" + to_string(expB);
+  }
+
+  return resultado;
+}
+
+void imprimirFila(int n, int k) {
+  if (k > n) {
+    return;
+  }
+
+  int coef = obtenerCoeficiente(n, k);
+  int expA = n - k;
+  int expB = k;
+
+  cout << formatearTermino(coef, expA, expB, k == 0);
+
+  imprimirFila(n, k + 1);
+}
+
+void imprimirTorre(int nivelActual, int nivelMaximo, binomy bin) {
+
+  if (nivelActual > nivelMaximo) {
+    return;
+  }
+
+  cout << "(a + b)^" << nivelActual << " = ";
+
+  imprimirFila(nivelActual, 0);
+
+  cout << '\n';
+
+  bin.potencia = nivelActual;
+  bin.state.push(make_tuple(nivelActual, "a", "b", "+", "=", nivelActual));
+
+  imprimirTorre(nivelActual + 1, nivelMaximo, bin);
+}
+
+void pila() {}
 
 void menu() {
   int opcion = 0;
@@ -35,7 +90,14 @@ void menu() {
     cout << "3. Salir" << endl;
     cin >> opcion;
     if (opcion == 1) {
-      piramide();
+      int n;
+      cout << "numero de niveles: ";
+      cin >> n;
+      if (n < 10) {
+        binomy bin;
+        imprimirTorre(0, n, bin);
+        counter++;
+      }
     } else if (opcion == 2) {
       pila();
     } else {
